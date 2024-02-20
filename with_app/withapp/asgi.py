092d0a1b53381b61e1432db169a7319d3389e1f9
joon719 <dlgkrwns719@gmail.com>
2024-02-20 11:14:02 +0900

    change app name

--- with_app/withapp/asgi.py
+++ with_app/withapp/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "with_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "withapp.settings")
 
 application = get_asgi_application()
--- with_app/withapp/wsgi.py
+++ with_app/withapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "with_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "withapp.settings")
 
 application = get_wsgi_application()
