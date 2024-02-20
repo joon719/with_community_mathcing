e0158f08bbcf8f7da4afaea1088ba7d01606c099
joon719 <dlgkrwns719@gmail.com>
2024-02-20 15:55:42 +0900

    31:49 까지

--- with_app/api/views.py
+++ with_app/api/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def getRoute(request):
+    routes = [
+        {
+            'Endpoint' : '/withapp/' ,
+            'method' : 'GET' ,
+            'body' : None ,
+            'description' : 'Returns an array of notes'
+        },
+        {
+            'Endpoint' : '/withapp/id' ,
+            'method' : 'GET' ,
+            'body' : None ,
+            'description' : 'Returns a single note object'
+        },
+        {
+            'Endpoint' : '/withapp/create/' ,
+            'method' : 'POST' ,
+            'body' : {'body' : ""} ,
+            'description' : 'Creates new note with data sent in post req'
+        },
+        {
+            'Endpoint' : '/withapp/id/update/' ,
+            'method' : 'PUT' ,
+            'body' : {'body' : ""} ,
+            'description' : 'Create an existing note with data sent'
+        },
+        {
+            'Endpoint' : '/withapp/id/delete/' ,
+            'method' : 'DELETE' ,
+            'body' : None ,
+            'description' : 'Deletes and exiting note'
+        },
+    ]
+    return Response(routes)
